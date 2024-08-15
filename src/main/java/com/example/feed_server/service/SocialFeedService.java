eeb1ca0ed6ec685ffa23325f8cb84c72d6526c43
iamseung <seesungseok@naver.com>
2024-08-16 00:42:31 +0900

    Terminal EventDriven 고도화 진행
    - Kafka 적용, 의존성 추가 및 Topic 정의
    - 서비스 로직에 메세지 Sending 추가
    - 피드 갱신, refresh 기능 추가

--- src/main/java/com/example/feed_server/service/SocialFeedService.java
+++ src/main/java/com/example/feed_server/service/SocialFeedService.java
@@ -1,13 +1,17 @@
 package com.example.feed_server.service;
 
+import com.example.feed_server.dto.FeedInfo;
 import com.example.feed_server.dto.FeedRequest;
 import com.example.feed_server.dto.UserInfo;
 import com.example.feed_server.entity.SocialFeed;
 import com.example.feed_server.repository.SocialFeedRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatusCode;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestClient;
@@ -18,13 +22,18 @@ import java.util.List;
 public class SocialFeedService {
     private final Logger log = LoggerFactory.getLogger(this.getClass().getSimpleName());
     private SocialFeedRepository feedRepository;
+    private KafkaTemplate<String, String> kafkaTemplate;
+    private ObjectMapper objectMapper;
 
     @Value("${sns.user-server}")
     private String userServiceUrl;
     private RestClient restClient = RestClient.create();
 
-    public SocialFeedService(SocialFeedRepository feedRepository) {
+    public SocialFeedService(SocialFeedRepository feedRepository, KafkaTemplate<String, String> kafkaTemplate, ObjectMapper objectMapper) {
         this.feedRepository = feedRepository;
+        this.kafkaTemplate = kafkaTemplate;
+        // Spring 에서 제공하는 data 기능이 포함된 objectMapper 를 제공받아 사용
+        this.objectMapper = objectMapper;
     }
 
     public List<SocialFeed> getAllFeeds() {
@@ -46,7 +55,35 @@ public class SocialFeedService {
 
     @Transactional
     public SocialFeed createFeed(FeedRequest feed) {
-        return feedRepository.save(new SocialFeed(feed));
+        SocialFeed savedFeed = feedRepository.save(new SocialFeed(feed));
+
+        // 사용자 조회
+        UserInfo uploader = getUserInfo(savedFeed.getUploaderId());
+        FeedInfo feedInfo = new FeedInfo(savedFeed, uploader.getUsername());
+
+        try {
+            kafkaTemplate.send("feed.created", objectMapper.writeValueAsString(feedInfo));
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+
+        return savedFeed;
+    }
+
+    // feed 갱신
+    public void refreshAllFeeds() {
+        List<SocialFeed> feeds = getAllFeeds();
+
+        for(SocialFeed feed : feeds) {
+            UserInfo uploader = getUserInfo(feed.getUploaderId());
+            FeedInfo feedInfo = new FeedInfo(feed, uploader.getUsername());
+
+            try {
+                kafkaTemplate.send("feed.created", objectMapper.writeValueAsString(feedInfo));
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 
     // user-server 로 부터 회원 정보 조회, RestClient
